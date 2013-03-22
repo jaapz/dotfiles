d9d41412ca5bda28781368e4afd4377e68473dfd
Jaap Broekhuizen <jaapz.b@gmail.com>
2013-03-22 14:09:21 +0100

    Add chrome and (oh my) zsh.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 # Installs several essential packages and links dotfiles
 # to their appropriate places in the filesystem.
 
+# We seriously need these packages for some commands in this script.
+apt-get install git-core bzr mercurial
+
 # All packages that need to be installed at the end of
 # this script.
 packages=""
@@ -21,6 +24,16 @@ packages="$packages mopidy ncmpc"
 # Conky.
 ln -s $PWD/conky/conkyrc ~/.conkyrc
 packages="$packages conky"
+
+# Chrome.
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+packages="$packages google-chrome-beta"
+
+# Zsh + oh-my-zsh.
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+ln -s $pwd/zsh/zshrc ~/.zshrc
+packages="$packages zsh"
 
 # TODO: install virtualenv, virtualenvwrapper
 
