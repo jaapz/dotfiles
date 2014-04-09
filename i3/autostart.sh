f3098dbaad295ed5541a5ab515c1899d725308d1
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-04-09 12:58:29 +0200

    Tweaked i3 a bit.
    Renamed workspaces to something sortable.
    Made sure to execute needed services when loading i3.

---
+++ i3/autostart.sh
@@ -0,0 +1,28 @@
+# Autostart script executed after i3 config is loaded. Largely stolen from the
+# default crunchbang openbox autostart script located in
+# ~/.config/openbox/autostart.
+
+## GNOME PolicyKit and Keyring
+eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+
+## Volume control for systray
+(sleep 2s && pnmixer) &
+
+## Volume keys daemon
+xfce4-volumed &
+
+## Enable power management
+xfce4-power-manager &
+
+## Start Thunar Daemon
+thunar --daemon &
+
+## Start xscreensaver
+xautolock -time 10 -locker 'i3lock -c 111111' &
+
+## Set keyboard settings - 250 ms delay and 25 cps (characters per second) repeat rate.
+## Adjust the values according to your preferances.
+xset r rate 250 25 &
+
+## Turn on/off system beep
+xset b off &
