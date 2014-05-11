ea46327e7dacd9aeed42d4231e52fadf716dad9d
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-05-11 13:33:54 +0200

    Updated mutt and offlineimap configs
    Added bin/offlineimap.py which decrypts a file that was encrypted using a
    private rsa key. This is useful for offlineimap configuration where you would
    have to provide a password in the config file.

---
+++ bin/offlineimap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+
+
+def unencrypt_password(filename):
+    """ Unencrypt the given file which contains the password.
+
+    The file should be encrypted using:
+        openssl rsa -in ~/.ssh/id_rsa -pubout > ~/.ssh/id_rsa.pub.pem
+        openssl rsautl -encrypt -pubin -inkey ~/.ssh/id_rsa.pub.pem > ~/outputfile
+    """
+    return os.popen("cat %s | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa" % filename).read().rstrip()
