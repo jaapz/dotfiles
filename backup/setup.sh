4645ea0e97ac5457dffd6af0963469784ae329c3
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-12-09 11:53:00 +0100

    Update to backup script

--- backup/setup.sh
+++ backup/setup.sh
@@ -2,7 +2,9 @@
 
 # This script simply adds a crontab entry for a 2-minute backup through unison.
 # It installs unison too. Unison syncs the files bi-directionally.
+# You should have a backup ssh key created in /home/jaapz/.ssh/backup/id_rsa
+# (generated using ssh-keygen -t rsa).
 
 sudo apt-get install unison
 
-{ crontab -l -u jaapz; echo '*/2 * * * * unison ~/synced ssh://broekhuizen.nu//home/jaapz/backup -auto -batch'; } | crontab -u jaapz -
+{ crontab -l -u jaapz; echo '*/2 * * * * unison -auto -batch -confirmbigdel=false -sshargs '-i /home/jaapz/.ssh/backup/id_rsa' ~/synced ssh://broekhuizen.nu//home/jaapz/backup'; } | crontab -u jaapz -
