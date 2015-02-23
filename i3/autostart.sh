3f4f6c1c0781156879aaba63e9567142a0e966ae
Jaap Broekhuizen <jaapz.b@gmail.com>
2015-02-23 22:24:11 +0100

    replace i3lock with slock

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -18,7 +18,7 @@ xfce4-power-manager &
 thunar --daemon &
 
 ## Start xscreensaver
-xautolock -time 10 -locker 'i3lock -c 111111' &
+xautolock -time 10 -locker 'slock' &
 
 ## Set keyboard settings - 250 ms delay and 25 cps (characters per second) repeat rate.
 ## Adjust the values according to your preferances.
